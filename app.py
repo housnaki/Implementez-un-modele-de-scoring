f2b9b31f6262072270e8e7cff0359c8649ce6ce2
housnaki <kouidri-hi@hotmail.fr>
2021-12-26 17:29:06 +0100

    “Ajout”

---
+++ app.py
@@ -0,0 +1,244 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 25 17:49:40 2021
+ 
+@author: Administrator
+"""
+
+import os
+import numpy as np
+import pandas as pd
+import streamlit as st
+from sklearn.model_selection import cross_validate
+from lightgbm import LGBMClassifier
+import plotly.express as px
+import pickle
+import shap
+import streamlit.components.v1 as components
+from interpret import show
+from interpret.blackbox import ShapKernel
+st.image('https://www.anaf.fr/wp-content/uploads/2020/09/OpenClassroom_LOGO.png', width=800)
+
+st.header('Home Credit Default Risk Dashboard')
+
+def histogram(df, x='str', legend=True, client=None): 
+    '''client = [df_test, input_client] '''
+    if x == "TARGET":
+        fig = px.histogram(df,
+                        x=x,
+                        color="TARGET",
+                        width=300,
+                        height=200,
+                        category_orders={"TARGET": [1, 0]})
+        fig.update_xaxes(showticklabels=False)
+        fig.update_layout(margin=dict(l=10, r=10, t=10, b=50))
+    else:
+        fig = px.histogram(df,
+                x=x,
+                color="TARGET",
+                width=300,
+                height=200,
+                category_orders={"TARGET": [1, 0]},
+                barmode="group",
+                histnorm='percent')
+        fig.update_layout(margin=dict(l=10, r=10, t=10, b=10))
+    if legend == True:
+        fig.update_layout(legend=dict(yanchor="top",xanchor="right"))
+    else:
+        fig.update_layout(showlegend=False)
+    if client: 
+        client_data = client[0][client[0].SK_ID_CURR ==  client[1]]
+        vline = client_data[x].to_numpy()[0]
+        print(vline)
+        
+        fig.add_vline(x=vline, line_width=3, line_dash="dash", line_color="black")
+    return fig  
+
+def reduce_memory_usage(df):  
+  
+    numeric2reduce = ["int16", "int32", "int64", "float64"]
+    for col, col_type in df.dtypes.iteritems():
+        if col_type == "object":
+            df[col] = df[col].astype("category")
+        elif col_type in numeric2reduce:
+            downcast = "integer" if "int" in str(col_type) else "float"
+            df[col] = pd.to_numeric(df[col], downcast=downcast)
+    return df 
+
+
+
+def get_data(finename):
+    df = pd.read_csv(finename,
+                    sep=',',
+                    low_memory=False,
+                    verbose=False,
+                    nrows = 20000,
+                    encoding='ISO-8859-1',
+                    dtype={'Special': 'object'}
+                    )
+    
+    df = reduce_memory_usage(df)
+    return df
+
+# Managing data import/export
+
+PATH = 'C:\\Users\\Administrator\\' # os.getcwd()+'/' # change for online app or local
+FILENAME_TRAIN = PATH+'train_Xy_sample.csv' # sample of train set for online version 25MB
+FILENAME_TEST = PATH+'X_test_sample.csv'
+df_description=get_data( PATH +"HomeCredit_columns_description.csv")
+df_train = get_data(FILENAME_TRAIN) # load trainset data in a df
+df_test = get_data(FILENAME_TEST)
+FILENAME_MODEL = PATH+'finalized_model.sav'
+
+  
+sb = st.sidebar # add a side bar
+
+sb.image('https://user.oc-static.com/upload/2019/02/25/15510866018677_logo%20projet%20fintech.png', width=280)
+
+sb.markdown('**Choose a client ID:**')
+np.random.seed(12) # one major change is that client is directly asked as input since sidebar
+label_test = df_test['SK_ID_CURR'].sample(200).sort_values()
+radio = sb.radio('', ['Random client ID', 'Type client ID'])
+
+if radio == 'Random client ID': # Choice choose preselected seed13 or a known client ID
+    input_client = sb.selectbox('Select random client ID', label_test)
+if radio == 'Type client ID':
+    input_client = int(sb.text_input('Type client ID', value=113798))
+
+sb.markdown('**Navigation**')
+rad = sb.radio('', [' Home', 
+' Client data',
+'Client prediction'])
+
+# defining containers of the app
+header = st.container()
+dataset = st.container()
+eda = st.container()
+model_training = st.container()
+model_predict = st.container()
+
+from PIL import Image
+
+image1 = Image.open('C:\\Users\\Administrator\\Global_feature_importance.png')
+
+
+if rad == ' Home': # with this we choose which container to display on the screen
+    with eda: 
+
+        
+        st.subheader("Here's the test dataframe.")
+        max_row = st.slider("Select at many row you wanna visualize", value=1000, min_value=1, max_value=len(df_test)) 
+        st.write(df_test.head(max_row)) 
+
+        st.subheader("Here's the dataframe_columns_description.")
+        max_row = st.slider("Select at many row you wanna visualize", value=218, min_value=1, max_value=len(df_description)) 
+        st.write(df_description.head(max_row))
+        
+        
+        st.subheader("Here's the global features importance of data.")        
+        st.image(image1, width=700)
+        
+        st.header("**Overview of exploratory data analysis.** \n ----")
+        st.subheader("Plotting distributions of target and some features.")
+ 
+        
+        col1, col2, col3 = st.columns(3) # 3 cols with histogram = home-made func
+        col1.plotly_chart(histogram(df_train, x='TARGET'), use_container_width=True)
+        col2.plotly_chart(histogram(df_train, x='CODE_GENDER'), use_container_width=True)
+        col3.plotly_chart(histogram(df_train, x='PAYMENT_RATE'), use_container_width=True)       
+
+        col1, col2, col3 = st.columns(3) # 3 cols with histogram = home-made func
+        col1.plotly_chart(histogram(df_train, x='EXT_SOURCE_1'), use_container_width=True)
+        col2.plotly_chart(histogram(df_train, x='EXT_SOURCE_2'), use_container_width=True)
+        col3.plotly_chart(histogram(df_train, x='EXT_SOURCE_3'), use_container_width=True)       
+
+        col1, col2, col3 = st.columns(3) # 3 cols with histogram = home-made func
+        col1.plotly_chart(histogram(df_train, x='DAYS_BIRTH'), use_container_width=True)
+        col2.plotly_chart(histogram(df_train, x='DAYS_EMPLOYED'), use_container_width=True)
+        col3.plotly_chart(histogram(df_train, x='AMT_ANNUITY'), use_container_width=True)       
+
+if rad == ' Client data':  
+    with eda:
+        st.header("**Client's data.** \n ----")
+        # retrieving whole row of client from sidebar input ID
+        client_data = df_test[df_test.SK_ID_CURR == input_client]
+        #client_data = client_data.drop(['SK_ID_CURR'])  
+
+        st.subheader(f"**Client ID: {input_client}.**")
+        # plotting features from train set, with client's data as dashed line (client!=None in func)
+        st.subheader("Ranking client in some features.")      
+        col1, col2, col3 = st.columns(3)
+        col1.plotly_chart(histogram(df_train, x='EXT_SOURCE_1', client=[df_test, input_client]), use_container_width=True)
+        col2.plotly_chart(histogram(df_train, x='EXT_SOURCE_2', client=[df_test, input_client]), use_container_width=True)
+        col3.plotly_chart(histogram(df_train, x='EXT_SOURCE_3', client=[df_test, input_client]), use_container_width=True)
+
+        col1, col2, col3 = st.columns(3)
+        col1.plotly_chart(histogram(df_train, x='DAYS_BIRTH', client=[df_test, input_client]), use_container_width=True)
+        col2.plotly_chart(histogram(df_train, x='DAYS_EMPLOYED', client=[df_test, input_client]), use_container_width=True)
+        col3.plotly_chart(histogram(df_train, x='AMT_ANNUITY', client=[df_test, input_client]), use_container_width=True)
+       
+        st.subheader("More information about this client.")
+        # st.subheader("More information about this client.")
+        # displaying values from a dropdown (had issues with NaNs that's why .dropna())
+
+        info = st.selectbox('What info?', client_data.columns.sort_values())     
+        info_print = client_data[info].to_numpy()[0]
+
+        st.subheader(info_print)
+        # displaying whole non NaNs row
+        st.write("All client's data.")
+        st.write(client_data)
+
+
+modelEssaiP7= 'finalized_model.sav'
+client_data = df_test[df_test.SK_ID_CURR == input_client]
+
+
+def st_shap(plot, height=None):
+    shap_html = f"<head>{shap.getjs()}</head><body>{plot.html()}</body>"
+    components.html(shap_html, height=height)
+
+df_test2=df_test.drop(["SK_ID_CURR"],1)
+
+feat_list= df_test2.select_dtypes(include=np.number)
+
+if rad == 'Client prediction': 
+    with model_predict:
+
+        col1, col2 = st.columns(2)
+        col1.markdown(f'**Client ID: {input_client}**')
+
+        if col2.button('Predict & plot!'):
+
+            try: 
+                model = pickle.load(open(FILENAME_MODEL, 'rb'))
+            except:
+                raise 'You must train the model first.'
+            # finding client row index in testset
+            idx = df_test.SK_ID_CURR[df_test.SK_ID_CURR == input_client].index
+            client = df_test2.iloc[idx, :]
+            y_prob = model.predict_proba(client)
+
+             
+            if (y_prob).T[1] < (y_prob.T)[0]:
+                st.subheader(f"**Probability of successful payment.**")
+            else:
+                st.subheader(f"**Probability of Failure payment.**")
+            # plotting pie plot for proba, finding good h x w was a bit tough
+            fig = px.pie(values=(pd.DataFrame(y_prob)).loc[0], names=["0","1"])
+            st.plotly_chart(fig, use_container_width=True)
+
+            st.subheader(f"**SHAP explanation force plots for the client.**")
+
+            explainer = shap.TreeExplainer(model)
+            shap_values = explainer.shap_values(client)
+            shap.initjs()
+            st_shap(shap.force_plot(explainer.expected_value[1], shap_values[1], client))
+
+            st.subheader(f"**SHAP explanation summary plots for the client.**")
+
+
+            st.set_option('deprecation.showPyplotGlobalUse', False)
+            st.pyplot(shap.summary_plot(shap_values[1], client),showPyplotGlobalUse = False)
+
+sb.markdown('**By Housna KOUIDRI**') 
